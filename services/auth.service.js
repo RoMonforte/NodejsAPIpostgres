c4d548a489a20c548c9eab7d37f6098994e0d5e1
Rodrigo Monforte <rodrigomonforte94@gmail.com>
2022-09-15 01:43:08 -0500

    changing password with a token and send emails

--- services/auth.service.js
+++ services/auth.service.js
@@ -34,11 +34,29 @@ class AuthService {
         token
       };
   }
-  async sendMail(email) {
+
+
+  async sendRecovery(email) {
     const user = await service.findByEmail(email);
     if(!user) {
       throw boom.unauthorized();
     }
+    const payload = {sub: user.id};
+    const token = jwt.sign(payload, config.jwtSecret, {expiresIn: '15min'});
+    const link = `http://myfrontend.com/recovery?token=${token}`;
+    await service.update(user.id, {recoveryToken: token});
+    const mail = {
+      from: '"emmett.weber@ethereal.email', // sender address
+      to: `${user.email}`,
+      subject: "Recovery password",
+      html: `<b>Ingresa a este link => ${link}</b>`,
+    }
+   const rta = await this.sendMail(mail)
+   return rta;
+  }
+
+  async sendMail(infomail) {
+
     const transporter = nodemailer.createTransport({
       host: "smtp.ethereal.email",
       secure: false,
@@ -48,14 +66,23 @@ class AuthService {
           pass: 'wWn2PzKBwUhsUbEnEB'
       }
     });
-    await transporter.sendMail({
-      from: '"emmett.weber@ethereal.email', // sender address
-      to: `${user.email}`, // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
+    await transporter.sendMail(infomail);
     return {message: 'mail sent'};
+  }
+
+  async changePassword(token, newPassword) {
+    try {
+      const payload = jwt.verify(token, config.jwtSecret);
+      const user = await service.findOne(payload.sub);
+      if (user.recoveryToken !== token) {
+        throw boom.unauthorized();
+      }
+      const hash = await bcrypt.hash(newPassword, 10);
+      await service.update(user.id, {recoveryToken: null, password: hash});
+      return {message: 'password changed'};
+    } catch (error) {
+      throw boom.unauthorized()
+    }
   }
 }
 
